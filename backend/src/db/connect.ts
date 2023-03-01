7842f95d68df0a26623f9eb8d92155d1fa92d108
Joseph Adamson <josephowenadamson@gmail.com>
2023-03-01 19:36:32 +0000

    moved repo after accidental npm force audit

--- backend/src/db/connect.ts
+++ backend/src/db/connect.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async (url: string) => {
     try {
         await mongoose.connect(url);
-        console.log("Successfully connect to mongo database");
+        console.log("Successfully connected to mongo database");
     } catch (error) {
         console.error(error);
     }
